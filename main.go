5b07327831aee90c18da0d99adfae9818b84258c
Puskar Basu <puskar@turbot.com>
2023-11-30 15:57:59 +0530

    Move qual filtering into main.go

--- main.go
+++ main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"context"
-	"encoding/csv"
+	encoding_csv "encoding/csv"
 	"fmt"
+	"log"
 	"os"
 	"sort"
-	"strings"
+	"strconv"
+	"time"
 
 	"github.com/golang/protobuf/ptypes"
-	"github.com/turbot/steampipe-plugin-aws/aws"
+	"github.com/hashicorp/go-hclog"
+	aws "github.com/turbot/steampipe-plugin-aws/aws"
+	"github.com/turbot/steampipe-plugin-sdk/v5/anywhere"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/logging"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/reflect/protoreflect"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	filter2 "github.com/turbot/steampipe-plugin-sdk/v5/filter"
+	"github.com/turbot/steampipe-plugin-sdk/v5/sperr"
 	"github.com/turbot/steampipe/pkg/ociinstaller"
 )
 
@@ -28,8 +36,9 @@ var connection = pluginAlias
 type displayRowFunc func(row *proto.ExecuteResponse, columns []string)
 
 func main() {
+	setupLogger(pluginAlias)
 	rootCmd := &cobra.Command{
-		Use:   "spdump",
+		Use:   "aws_dump TABLE_NAME [flags]",
 		Short: "Steampipe data Dump",
 		Run:   executeCommand,
 		Args:  cobra.ExactArgs(1),
@@ -173,11 +182,8 @@ func executeQuery(tableName string, conectionName string, columns []string, qual
 		},
 	}
 	ctx := context.Background()
-	stream := plugin.NewLocalPluginStream(ctx)
-	err := pluginServer.CallExecute(req, stream)
-	if err != nil {
-		fmt.Println("Error in call execute")
-	}
+	stream := anywhere.NewLocalPluginStream(ctx)
+	pluginServer.CallExecuteAsync(req, stream)
 	for {
 
 		response, err := stream.Recv()
@@ -196,21 +202,18 @@ var rowCount = 0
 
 func displayCSVRow(displayRow *proto.ExecuteResponse, columns []string) {
 	row := displayRow.Row
+	selectColumns := viper.GetStringSlice("select")
 
+	// Process each column and store values in a map
 	res := make(map[string]string, len(row.Columns))
 	for columnName, column := range row.Columns {
-		// extract column value as interface from protobuf message
-		// var i error
 		var val interface{}
 		if bytes := column.GetJsonValue(); bytes != nil {
 			val = string(bytes)
 		} else if timestamp := column.GetTimestampValue(); timestamp != nil {
-			// convert from protobuf timestamp to a RFC 3339 time string
 			val = ptypes.TimestampString(timestamp)
 		} else {
-			// get the first field descriptor and value (we only expect column message to contain a single field
 			column.ProtoReflect().Range(func(descriptor protoreflect.FieldDescriptor, v protoreflect.Value) bool {
-				// is this value null?
 				if descriptor.JSONName() == "nullValue" {
 					val = nil
 				} else {
@@ -219,43 +222,303 @@ func displayCSVRow(displayRow *proto.ExecuteResponse, columns []string) {
 				return false
 			})
 		}
-		if len(viper.GetStringSlice("select")) != 0 {
-			if slices.Contains(viper.GetStringSlice("select"), columnName) {
-				res[columnName] = fmt.Sprintf("%v", val)
-			}
-		} else {
-			res[columnName] = fmt.Sprintf("%v", val)
-		}
+		res[columnName] = fmt.Sprintf("%v", val)
 	}
 
-	var dataHeader string
-	var dataRows string
-	writer := csv.NewWriter(os.Stdout)
-
+	// Prepare CSV writer
+	writer := encoding_csv.NewWriter(os.Stdout)
 	defer writer.Flush()
+
+	// Write headers
 	if rowCount == 0 {
-		dataHeader = strings.Join(columns, ",")
-		fields := strings.Split(dataHeader, ",")
-		writer.Write(fields)
+		if len(selectColumns) > 0 {
+			// Write headers based on selectColumns
+			writer.Write(selectColumns)
+		} else {
+			// Write all headers
+			writer.Write(columns)
+		}
 		writer.Flush()
 
 		if err := writer.Error(); err != nil {
-    		fmt.Println(err)
+			fmt.Println(err)
 		}
 	}
 
 	rowCount++
 
-	colVals := make([]string, len(columns))
-	for i, c := range columns {
-		colVals[i] = res[c]
+	// Generate row data
+	var colVals []string
+	if len(selectColumns) > 0 {
+		colVals = make([]string, len(selectColumns))
+		for i, columnName := range selectColumns {
+			colVals[i], _ = res[columnName] // Using _ to ignore whether columnName is present in res
+		}
+	} else {
+		colVals = make([]string, len(columns))
+		for i, columnName := range columns {
+			colVals[i], _ = res[columnName]
+		}
 	}
-	dataRows = strings.Join(colVals, ",")
-	fields := strings.Split(dataRows, ",")
-	writer.Write(fields)
+
+	// Write the row data
+	writer.Write(colVals)
 	writer.Flush()
 
+	// Handle potential errors from the writer
 	if err := writer.Error(); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func filterStringToQuals(raw string, tableSchema *proto.TableSchema) (map[string]*proto.Quals, error) {
+	columnMap := tableSchema.GetColumnMap()
+	keyColumns := tableSchema.GetAllKeyColumns()
+
+	parsed, err := filter2.Parse("", []byte(raw))
+	if err != nil {
+		log.Printf("err %v", err)
+		return nil, sperr.New("failed to parse 'where' property: %s", err.Error())
+	}
+
+	// convert table schema into a column map
+
+	filter := parsed.(filter2.ComparisonNode)
+	log.Println(filter)
+	var qual *proto.Qual
+	var column string
+
+	switch filter.Type {
+
+	case "compare", "like":
+		codeNodes, ok := filter.Values.([]filter2.CodeNode)
+		if !ok {
+			return nil, fmt.Errorf("failed to parse filter")
+		}
+		if len(codeNodes) != 2 {
+			return nil, fmt.Errorf("failed to parse filter")
+		}
+
+		column = codeNodes[0].Value
+		value := codeNodes[1].Value
+		operator := filter.Operator.Value
+
+		// map the operator
+		mappedOperator := mapOperator(operator)
+
+		// validate this qual
+		// - the column exists in the table
+		// - the column is a key column
+		// - the operator is supported
+		if err := validateQual(column, mappedOperator, columnMap, keyColumns); err != nil {
+			return nil, err
+		}
+
+		// convert the value string into a qual
+		columnType := columnMap[column].Type
+		qualValue, err := stringToQualValue(value, columnType)
+		if err != nil {
+			return nil, err
+		}
+
+		qual = &proto.Qual{
+			FieldName: column,
+			Operator:  &proto.Qual_StringValue{mappedOperator},
+			Value:     qualValue,
+		}
+
+	case "in":
+		if filter.Operator.Value == "not in" {
+			return nil, fmt.Errorf("failed to convert 'where' arg to qual - 'not in' is not supported")
+		}
+		codeNodes, ok := filter.Values.([]filter2.CodeNode)
+		if !ok || len(codeNodes) < 2 {
+			return nil, fmt.Errorf("failed to parse filter")
+		}
+		column = codeNodes[0].Value
+		operator := "="
+
+		// map the operator
+		mappedOperator := mapOperator(operator)
+
+		// validate this qual
+		// - the column exists in the table
+		// - the colummn is a key column
+		// - the operator is supported
+		if err := validateQual(column, mappedOperator, columnMap, keyColumns); err != nil {
+			return nil, err
+		}
+
+		// Build look up of values
+		values := make(map[string]struct{}, len(codeNodes)-1)
+		for _, c := range codeNodes[1:] {
+			values[c.Value] = struct{}{}
+		}
+
+		// Convert these raw values into a qual
+		columnType := columnMap[column].Type
+		qualValue, err := stringToQualListValue(maps.Keys(values), columnType)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a Qual slice for the field and add the Qual to it
+		qual = &proto.Qual{
+			FieldName: column,
+			Operator:  &proto.Qual_StringValue{mappedOperator},
+			Value:     qualValue,
+		}
+
+	default:
+		return nil, fmt.Errorf("failed to convert 'where' arg to qual")
+
+	}
+
+	if qual == nil {
+		// unexpected
+		return nil, fmt.Errorf("failed to convert 'where' arg to qual")
+	}
+
+	qualmap := make(map[string]*proto.Quals)
+	qualmap[column] = &proto.Quals{Quals: []*proto.Qual{qual}}
+
+	return qualmap, nil
+}
+
+// validate this qual
+// - the column exists in the table
+// - the colummn is a key column
+// - the operator is supported
+func validateQual(column, operator string, columnMap map[string]*proto.ColumnDefinition, quals []*proto.KeyColumn) error {
+	// does the column exists in the table
+	_, ok := columnMap[column]
+	if !ok {
+		return fmt.Errorf("column %s does not exist", column)
+	}
+
+	unsupportedOperator := false
+	// is the column is a key column
+	for _, keyColumn := range quals {
+		// is this key column for the target column
+		if keyColumn.Name == column {
+			// check the operator is supported
+			if isOperatorSupported(keyColumn.Operators, operator) {
+				// ok this qual is valid
+				return nil
+			} else {
+				unsupportedOperator = true
+			}
+		}
+	}
+	if unsupportedOperator {
+		return fmt.Errorf("key column for '%s' does not support operator '%s'", column, operator)
+	}
+	return fmt.Errorf("there is no key column defined for column '%s'", column)
+}
+
+func stringToQualValue(valueString string, columnType proto.ColumnType) (*proto.QualValue, error) {
+	result := &proto.QualValue{}
+	switch columnType {
+	case proto.ColumnType_BOOL:
+		b, err := strconv.ParseBool(valueString)
+		if err != nil {
+			return nil, err
+		}
+		result.Value = &proto.QualValue_BoolValue{BoolValue: b}
+	case proto.ColumnType_INT:
+		i, err := strconv.ParseInt(valueString, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		result.Value = &proto.QualValue_Int64Value{Int64Value: i}
+	case proto.ColumnType_DOUBLE:
+		f, err := strconv.ParseFloat(valueString, 64)
+		if err != nil {
+			return nil, err
+		}
+		result.Value = &proto.QualValue_DoubleValue{DoubleValue: f}
+	case proto.ColumnType_STRING:
+		result.Value = &proto.QualValue_StringValue{StringValue: valueString}
+	case proto.ColumnType_JSON:
+		result.Value = &proto.QualValue_JsonbValue{JsonbValue: valueString}
+	case proto.ColumnType_IPADDR:
+		// todo parse
+	case proto.ColumnType_CIDR:
+		// todo parse
+	case proto.ColumnType_INET:
+		// todo parse
+
+	case proto.ColumnType_DATETIME, proto.ColumnType_TIMESTAMP:
+		//t, err := time.Parse("Mon Jan 2 15:04:05 MST 2006", valueString)
+		//if err != nil{
+		//	return nil, err
+		//}
+		//result.Value = &proto.QualValue_TimestampValue{TimestampValue: t}
+		// todo parse
+	case proto.ColumnType_LTREE:
+		result.Value = &proto.QualValue_LtreeValue{LtreeValue: valueString}
+	}
+
+	if result.Value == nil {
+		return nil, fmt.Errorf("faile to convert value string")
+	}
+	return result, nil
+}
+
+func stringToQualListValue(values []string, columnType proto.ColumnType) (*proto.QualValue, error) {
+	res := &proto.QualValue{
+		Value: &proto.QualValue_ListValue{
+			ListValue: &proto.QualValueList{
+				Values: make([]*proto.QualValue, len(values)),
+			},
+		},
+	}
+	for i, v := range values {
+		qv, err := stringToQualValue(v, columnType)
+
+		if err != nil {
+			return nil, err
+		}
+		res.Value.(*proto.QualValue_ListValue).ListValue.Values[i] = qv
+	}
+	return res, nil
+}
+
+func setupLogger(plugin string) {
+	level := logging.LogLevel()
+	hcLevel := hclog.LevelFromString(level)
+
+	options := &hclog.LoggerOptions{
+		// make the name unique so that logs from this instance can be filtered
+		Name:       fmt.Sprintf("[%s]", plugin),
+		Level:      hcLevel,
+		Output:     os.Stderr,
+		TimeFn:     func() time.Time { return time.Now().UTC() },
+		TimeFormat: "2006-01-02 15:04:05.000 UTC",
+	}
+	logger := logging.NewLogger(options)
+	log.SetOutput(logger.StandardWriter(&hclog.StandardLoggerOptions{InferLevels: true}))
+	log.SetPrefix("")
+	log.SetFlags(0)
+}
+
+// mapOperator translates equivalent operator representations to a standard form.
+func mapOperator(operator string) string {
+	operatorMappings := map[string]string{
+		"like": "~~", // Map "like" to "~~"
+		// TODO PSKR: Add more mappings here as needed.
+	}
+
+	// Check if the operator is in the mapping, if so, return the mapped value.
+	if mappedOperator, ok := operatorMappings[operator]; ok {
+		return mappedOperator
+	}
+
+	// If no mapping is found, return the original operator.
+	return operator
+}
+
+func isOperatorSupported(keyColumns []string, mappedOperator string) bool {
+	// Check if the mapped operator is supported.
+	return slices.Contains(keyColumns, mappedOperator)
 }
