3dbe72604e46aa1a9076c950a3eae16778b1a860
Puskar Basu <puskar@turbot.com>
2024-06-12 14:50:24 +0530

    Remove release script, this should get created during the build process

--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export VERSION=$(go list -m -json github.com/turbot/steampipe-plugin-chaosdynamic | jq --raw-output '.Version | sub("^v"; "")')
-echo "VERSION set to $VERSION"
-goreleaser release --snapshot --rm-dist --skip=publish --skip=validate
